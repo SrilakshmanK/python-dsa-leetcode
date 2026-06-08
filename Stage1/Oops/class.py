fe51b5e9cd2bf04a8b978945916302e2cf487920
Srilakshman K <srilogan2612004@gmail.com>
2026-06-08 18:33:59 +0530

    Class completed

---
+++ Stage1/Oops/class.py
@@ -0,0 +1,12 @@
+# Class defenition
+# A class is a blueprint or template for creating a object .
+#Example program 
+
+class Dog:
+
+  def bark(self):
+    print("woof woof!!!")
+
+d1 = Dog()
+d1.bark()
+
