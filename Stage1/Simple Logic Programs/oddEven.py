244b0ef1fe35c3e032d5c5a0de002e2dab3cc9df
Srilakshman K <srilogan2612004@gmail.com>
2026-06-16 17:07:57 +0530

    Odd or Even Program

---
+++ Stage1/Simple Logic Programs/oddEven.py
@@ -0,0 +1,9 @@
+n = int(input("Enter the number:"))
+
+if(n<1):
+  print("0 and Negative values not accepted:")  
+elif(n%2==0):
+  print(f"{n} is a even number.")
+else :
+  print(f"{n} is a odd number.")  
+  
