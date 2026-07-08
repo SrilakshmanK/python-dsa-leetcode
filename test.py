59d034e3536dfa5b60fd0f1d14e133fdbe26f1bf
Srilakshman K <srilogan2612004@gmail.com>
2026-07-08 19:24:47 +0530

    DSA.zip were added

---
+++ test.py
@@ -0,0 +1,17 @@
+arr = [1 , 3 , 4 , 9 , 7]
+
+if arr[0] > arr[1]:
+    largest=arr[0]
+    second_largest=arr[1]
+else:
+    largest=arr[1]
+    second_largest=arr[0]
+
+for i in range (2,len(arr)):
+    if arr[i] > largest:
+        second_largest = largest
+        largest = arr[i]
+    elif arr[i] > second_largest:
+        second_largest = arr[i]
+
+print(second_largest)
